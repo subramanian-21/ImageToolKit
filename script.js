1eceb6f276c0d319e5a88baae15a0bbb2d9c1578
thugperfect <meenamani9444@gmail.com>
2023-07-11 10:36:12 +0000

    added js validation

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function ImageConvert(){
-    
+    let fileConvert = document.getElementById("fileconvert").value
+    let typeSelect = document.getElementById("filetype").value
+
+    if(!fileConvert || !typeSelect){
+        alert("Upload file and select Type")
+    }
+
+}
+function ImageCompresser(){
+    let fileCompress = document.getElementById("fileconvert").value
+    let compressSize = document.getElementById("filetype").value
+    if(!fileCompress || !compressSize)
+    {
+        alert("Upload file and Enter Size")
+    }
 }
