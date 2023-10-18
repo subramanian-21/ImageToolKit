3cd54b04190f3e599f5575ab2a964d76342a96e1
thugperfect <meenamani9444@gmail.com>
2023-10-18 19:12:52 +0530

    compresser logic complete

--- backend/controller/allConverters.js
+++ backend/controller/allConverters.js
@@ -51,7 +51,7 @@ const allController = {
     try {
       const imagePath = req.file.path;
       const image = await jimp.read(imagePath)
-      const sizePercentage = req.body.size
+      const quality = req.body.quality
       const imageName = req.file.originalname
       const imageSplitArray = imageName.split('.')
       const imageSplitArrayLength = imageSplitArray.length
@@ -59,9 +59,39 @@ const allController = {
 
       const convertedImageName = `converted.${imageFormat}`
       const convertedImagePath = 'converted/'+convertedImageName
-      
+      await jimp.quality(quality).writeAsync(convertedImagePath)
+      const convertedImageAbsolutePath = path.resolve(
+        __dirname,
+        "../converted",
+        convertedImageName
+      );
+
+     res.set("Content-Type", `image/${imageFormat}`);
+      res.sendFile(convertedImageAbsolutePath, (err) => {
+        if (err) {
+          console.error("File sending error:", err);
+          res.status(500).send("Error sending the file.");
+        } else {
+          console.log("send successfully");
+          fs.unlink(convertedImagePath, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted converted");
+            }
+          });
+          fs.unlink(imagePath, (err) => {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("deleted upload");
+            }
+          });
+        }
+      });
     } catch (error) {
-      
+      console.error("Image compression error:", error);
+      res.status(500).send("Image conversion failed.");
     }
   }
 };
