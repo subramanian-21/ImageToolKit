c9bf78e80928c93413cd14434c2914110a444eb7
thugperfect <meenamani9444@gmail.com>
2023-10-19 06:50:21 +0530

    navbar complete

--- frontend/src/app/directive/animate.directive.ts
+++ frontend/src/app/directive/animate.directive.ts
@@ -7,12 +7,12 @@ export class AnimateDirective {
 
   constructor(private e:ElementRef,private renderer:Renderer2) { }
   @HostListener('focus') inc(){
-    this.renderer.setStyle(this.e.nativeElement,"width","200px")
+    this.renderer.setStyle(this.e.nativeElement,"width","180px")
     this.renderer.addClass(this.e.nativeElement,'lengthIncrease')
     this.renderer.removeClass(this.e.nativeElement,'lengthDecrease')
   }
   @HostListener('blur') dec(){
-    this.renderer.setStyle(this.e.nativeElement,'width','150px')
+    this.renderer.setStyle(this.e.nativeElement,'width','130px')
     this.renderer.addClass(this.e.nativeElement,'lengthDecrease')
     this.renderer.removeClass(this.e.nativeElement,'lengthIncrease')
   }
